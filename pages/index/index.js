e94713f1e56cc4303914e54a72658bf7e6b33c40
haolong <hihaolong@gmail.com>
2020-01-30 01:36:56 -0500

    Now we are able to upload to the cloud database
    Miniprogram team really did a sloppy work that consequence in so many troubles using that sdk. So frustrated and want sleep.

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,30 +1,35 @@
-//index.js
+// miniprogram/pages/zealotime/zealotime.js
 //获取应用实例
 const app = getApp()
-
 Page({
+
+  /**
+   * Page initial data
+   */
   data: {
     motto: 'Hello World',
     userInfo: {},
+    myIntervalSize: 10000, //Every 10min is 6000000
     myLocationHistory: [], //[ [t1,latitude1,longitude1],[t2,la2,lo2],...,[] ]
+    latitude: 0,
+    longitude: 0,
+    myTimeSlotUTC: 158000,
     //userBluetooth: '____',
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
     let self = this
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -46,44 +51,71 @@ Page({
       })
     }
 
-    //console.log("aa",Date.now() / 100000,Date.now() % 100000)
-    //wait to a integer 10second
-    while (Date.now() % 10000 != 0) { 
-      //Wait
-    }
-    //Repeat and repeat
-    var myInterval2 = setInterval(function () {
-      var myTimeSlotUTC = Math.floor(Date.now() / 10000)
-      //Get user location
-      wx.getLocation({
-        type: 'gcj02',
-        isHighAccuracy: 'true',
-        highAccuracyExpireTime: '3000', //at least 3000 for performance
-        success(res) {
-          console.log(res)
-          var myLocationHistory = self.data.myLocationHistory
-          //console.log(myLocationHistory)
-          myLocationHistory.push([myTimeSlotUTC, res.latitude, res.longitude])
-          self.setData({
-            latitude: res.latitude,
-            longitude: res.longitude,
-            speed: res.speed,
-            accuracy: res.accuracy,
-            myLocationHistory: myLocationHistory
-          })
-          wx.setStorage({
-            key: String(myTimeSlotUTC),
-            data: [res.latitude, res.longitude],
-            success: function(res) {},
-            fail: function (res) {console.log(res)},
-            complete: function(res) {},
-          })
-        }
-      })
-    },10000) //Every 10min  6000000
-    //console.log("cc",Date.now() / 10000, Date.now() % 10000)
+    //Firstime in recording user location
+    this.myGetLocationNow()
 
-  getUserInfo: function(e) {
+    //var value = wx.getStorageSync('158036260')
+    //console.log("dataa2", value[0])
+    
+  },
+  
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+    let self = this
+    //Repeat every 10 minutes in recording user location
+    var myInterval = setInterval(function () {
+      //self.myGetLocationNow()
+    }, self.data.myIntervalSize)
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  },
+
+  
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
@@ -91,10 +123,79 @@ Page({
       hasUserInfo: true
     })
   },
-  navigateScan: function(e){
-    console.log(e)
-    wx.navigateTo({
-      url: '../scan/scan'
+  myGetLocationNow: function () {
+    self = this
+    var myTimeSlotUTC = Math.floor(Date.now() / self.data.myIntervalSize)
+    //Get user location
+    wx.getLocation({
+      type: 'gcj02',
+      isHighAccuracy: 'true',
+      highAccuracyExpireTime: '3000', //at least 3000 for performance
+
+      success(res) {
+        console.log(res)
+        //Record time slot and location
+        var myLocationHistory = self.data.myLocationHistory
+        //console.log(myLocationHistory)
+        myLocationHistory.push([myTimeSlotUTC, res.latitude, res.longitude, res.speed])
+        self.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          speed: res.speed,
+          accuracy: res.accuracy,
+          myLocationHistory: myLocationHistory
+        })
+        //Record time slot and location to local storage
+        wx.setStorage({
+          key: String(myTimeSlotUTC),
+          data: [res.latitude, res.longitude, res.speed],
+          success: function (res) { },
+          fail: function (res) { console.log("Failed in Storage", res) },
+          complete: function (res) { },
+        })
+      }
     })
+  },
+
+  //Upload one record a time to cloud:database:collection
+  myAddReport: function(){
+    var myStorageKeys = wx.getStorageInfoSync().keys
+    //console.log("myStorageKeys", key)
+
+    for (var key of myStorageKeys) {
+      //console.log("hey", key)
+      var myLocalRecord = wx.getStorageSync(key)
+      //console.log("myLocalRecord",myLocalRecord[0])
+
+      const db = wx.cloud.database()
+      db.collection('z_reports').add({
+        data: {
+          myTimeSlotUTC: key,
+          latitude:  myLocalRecord[0],
+          longitude: myLocalRecord[1],
+          speed:     myLocalRecord[2]
+        },
+        success: res => {
+          // 在返回结果中会包含新创建的记录的 _id
+          this.setData({
+            counterId: res._id,
+            count: 1
+          })
+          wx.showToast({
+            title: '新增记录成功',
+          })
+          console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+        },
+        fail: err => {
+          wx.showToast({
+            icon: 'none',
+            title: '新增记录失败'
+          })
+          console.error('[数据库] [新增记录] 失败：', err)
+        }
+      })
+    }
   }
+
+
 })
